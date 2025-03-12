e0cae085389a28d27629ee12ed18dcf1a7aff1f7
tech430 <kajanlingam909@gmail.com>
2025-03-12 15:05:12 +0530

    design

--- Weather/src/App.jsx
+++ Weather/src/App.jsx
@@ -3,13 +3,67 @@ import './App.css'
 import SearchIcon from "./assets/search.png";
 import CloudIcon from "./assets/cloud.jpg";
 import DrizzleIcon from "./assets/drizzle.png";
-import HuminityIcon from "./assets/humminity.png";
+import HumidityIcon from "./assets/humidity3.png";
 import ClearIcon from "./assets/clear.jpg";
 import SnowIcon from "./assets/snow.jpg";
-import WindIcon from "./assets/wind.png";
+import WindIcon from "./assets/wind2.png";
 
+const WeatherDetailes = ({icon,temp,city,country,lat,log,humidity,wind})=>{
+    return(
+      <>
+      <div className='image'>
+        <img src={icon} alt="Image" />
+      </div>
+      <div className="temp">{temp}<sup>o</sup>C</div>
+      <div className="location">{city}</div>
+      <div className="country">{country}</div>
+      <div className="cord">
+        <div>
+          <span className="lat">lattitute</span>
+          <span>{lat}</span>
+        </div>
+        <div>
+          <span className="log">longitude</span>
+          <span>{log}</span>
+        </div>
+      </div>
 
+      <div className="data-container">
+        <div className="element">
+          <img src={HumidityIcon} alt="humidity" className='icon' />
+          <div className="data">
+          <div className="humidity-presentage">{humidity}%</div>
+          <div className="text">Humidity</div>
+        </div>
+        </div>
+      
+        <div className="element">
+          <img src={WindIcon} alt="humidity" className='icon' />
+          <div className="data">
+          <div className="wind-presentage">{wind} km/h</div>
+          <div className="text">Wind Speed</div>
+        </div>
+        </div>
+        
+      </div>
+      
+      </>
+    );
+
+}
 function App() {
+  const [icon,setIcon] = useState(CloudIcon);
+  const [temp,setTemp] = useState(0);
+  const [city,setCity] = useState("Kandy");
+  const [country,setCountry] = useState("SriLanka");
+  const [lat,setLat] = useState(0);
+  const [log,setLog] = useState(0);
+  const [humidity,setHumidity] = useState(0);
+  const [wind,setWind] = useState(0);
+
+
+
+
   return (
     <>
       <div className='container'>
@@ -23,6 +77,8 @@ function App() {
           </div>
 
         </div>
+        <WeatherDetailes icon={icon} temp={temp} city={city} 
+        country={country} lat={lat} log ={log} humidity={humidity} wind={wind}/>
        
       </div>
     </>
